741dd1895d36521ef7e80c222c8580a4f6bf5331
unloco <amxmodx.unloco@gmail.com>
2023-07-23 04:06:40 +0100

    Add icons

---
+++ src/main/kotlin/net/prestalife/svirtual/Icons.java
@@ -0,0 +1,17 @@
+package net.prestalife.svirtual;
+
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class Icons {
+  private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
+    return IconManager.getInstance().loadRasterizedIcon(path, Icons.class.getClassLoader(), cacheKey, flags);
+  }
+  /** 16x16 */ public static final @NotNull Icon Page = load("icons/page.svg", -938751982, 0);
+}
--- src/main/kotlin/net/prestalife/svirtual/SveltePageNode.kt
+++ src/main/kotlin/net/prestalife/svirtual/SveltePageNode.kt
@@ -2,8 +2,6 @@ package net.prestalife.svirtual
 
 import com.intellij.ide.projectView.PresentationData
 import com.intellij.ide.projectView.impl.nodes.PsiFileNode
-import com.intellij.ui.IconManager
-import javax.swing.Icon
 
 class SveltePageNode(
     route: String,
@@ -12,14 +10,6 @@ class SveltePageNode(
 ) :
     PsiFileNode(psiFileNode.project, psiFileNode.value, viewSettings) {
 
-    private fun load(path: String, cacheKey: Int, flags: Int): Icon {
-        return IconManager.getInstance().loadRasterizedIcon(
-            path,
-            SveltePageNode::class.java.getClassLoader(), cacheKey, flags
-        )
-    }
-
-    private val pageIcon: Icon = load("icons/page.svg", -938751982, 0)
     private var route: String
 
     init {
@@ -29,9 +19,6 @@ class SveltePageNode(
 
     override fun updateImpl(data: PresentationData) {
         data.presentableText = "$route.svelte"
-        data.setIcon(pageIcon)
-//        data.background = Color(255, 0, 0 )
-        return
-//        super.updateImpl(data)
+        data.setIcon(Icons.Page)
     }
 }
