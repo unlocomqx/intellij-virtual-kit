e41b3e530ee8c5b40385701d0fe63ce7fac4a4b4
unloco <amxmodx.unloco@gmail.com>
2023-07-23 02:15:18 +0100

    Display svelte page icon

--- src/main/kotlin/net/prestalife/svirtual/SveltePageNode.kt
+++ src/main/kotlin/net/prestalife/svirtual/SveltePageNode.kt
@@ -2,7 +2,8 @@ package net.prestalife.svirtual
 
 import com.intellij.ide.projectView.PresentationData
 import com.intellij.ide.projectView.impl.nodes.PsiFileNode
-import java.awt.Color
+import com.intellij.ui.IconManager
+import javax.swing.Icon
 
 class SveltePageNode(
     route: String,
@@ -11,6 +12,14 @@ class SveltePageNode(
 ) :
     PsiFileNode(psiFileNode.project, psiFileNode.value, viewSettings) {
 
+    private fun load(path: String, cacheKey: Int, flags: Int): Icon {
+        return IconManager.getInstance().loadRasterizedIcon(
+            path,
+            SveltePageNode::class.java.getClassLoader(), cacheKey, flags
+        )
+    }
+
+    val pageIcon: Icon = load("icons/page.svg", -938751982, 0)
     private var route: String
 
     init {
@@ -19,7 +28,8 @@ class SveltePageNode(
     }
 
     override fun updateImpl(data: PresentationData) {
-        data.presentableText = route
+        data.presentableText = "$route.svelte"
+        data.setIcon(pageIcon)
 //        data.background = Color(255, 0, 0 )
         return
 //        super.updateImpl(data)
