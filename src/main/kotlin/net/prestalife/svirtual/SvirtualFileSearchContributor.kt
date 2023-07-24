68ddab26685e84eea8448a8e5a4cc5a8210c314b
unloco <amxmodx.unloco@gmail.com>
2023-07-24 17:31:42 +0100

    Add file search

--- src/main/kotlin/net/prestalife/svirtual/SvirtualFileSearchContributor.kt
+++ src/main/kotlin/net/prestalife/svirtual/SvirtualFileSearchContributor.kt
@@ -4,7 +4,6 @@ import com.intellij.navigation.ChooseByNameContributorEx
 import com.intellij.navigation.NavigationItem
 import com.intellij.openapi.util.Comparing
 import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.testFramework.utils.vfs.getPsiFile
 import com.intellij.util.Processor
 import com.intellij.util.indexing.FindSymbolParameters
 import com.intellij.util.indexing.IdFilter
@@ -21,7 +20,8 @@ class SvirtualFileSearchContributor : ChooseByNameContributorEx {
     ) {
         val files =
             TouchedFiles.list.filter { Comparing.strEqual(it.getUserData(TouchedFiles.VIRTUAL_FILE_NAME), name) }
-        files.forEach { if (!processor.process(it.getPsiFile(parameters.project))) return@forEach }
+        TouchedFiles.convertVirtualFilesToPsiFiles(parameters.project, files)
+            .forEach { if (!processor.process(it)) return@forEach }
     }
 }
 
--- src/main/kotlin/net/prestalife/svirtual/data/TouchedFiles.kt
+++ src/main/kotlin/net/prestalife/svirtual/data/TouchedFiles.kt
@@ -1,7 +1,10 @@
 package net.prestalife.svirtual.data
 
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
 
 class TouchedFiles {
     companion object {
@@ -16,6 +19,21 @@ class TouchedFiles {
                 file.putUserData(VIRTUAL_FILE_NAME, newName)
             }
             list.add(file)
+        }
+
+        fun convertVirtualFilesToPsiFiles(project: Project, files: Collection<VirtualFile?>): Collection<PsiFile> {
+            val psiFiles: MutableCollection<PsiFile> = HashSet()
+            var psiManager: PsiManager? = null
+            for (file in files) {
+                if (psiManager == null) {
+                    psiManager = PsiManager.getInstance(project)
+                }
+                val psiFile = psiManager.findFile(file!!)
+                if (psiFile != null) {
+                    psiFiles.add(psiFile)
+                }
+            }
+            return psiFiles
         }
     }
 }
