b485a86a7f8dfc3adfabaa597cde71bb33e583c3
unloco <amxmodx.unloco@gmail.com>
2023-07-28 12:41:34 +0100

    Revert Icons

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2023.2")
-    type.set("IU") // Target IDE Platform
+    version.set("2023.1.4")
+    type.set("PS") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
 }
