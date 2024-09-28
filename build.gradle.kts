946767521d172a0b75fe64abfee50be5857574e9
Tuni-Soft <amxmodx.unloco@gmail.com>
2024-09-28 16:54:51 +0100

    change ps version

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("LATEST-EAP-SNAPSHOT")
+    version.set("2024.2.3")
     type.set("PS") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
